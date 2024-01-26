239bcb4622de048f49e4820bfb5aa3f40f2196ad
unknown <you@example.com>
2024-01-26 22:00:02 +0100

    Edit Book

--- frontend/vite-project/src/pages/EditBook.jsx
+++ frontend/vite-project/src/pages/EditBook.jsx
@@ -1,7 +1,93 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+import BookService from "../services/BooksService";
+import BackButton from "../components/BackButton";
+import SpinerComponent from "../components/SpinerComponent";
+import { useNavigate, useParams } from "react-router-dom";
 
-function EditBook() {
-  return <div>EditBook</div>;
-}
+const EditBook = () => {
+  const [title, setTitle] = useState("");
+  const [author, setAuthor] = useState("");
+  const [publishYear, setPublishYear] = useState("");
+  const [loading, setLoading] = useState(false);
+  const navigate = useNavigate();
+  const { id } = useParams();
+
+  useEffect(() => {
+    setLoading(true);
+    BookService.editBook(id)
+      .then((res) => {
+        setTitle(res.data.title);
+        setAuthor(res.data.author);
+        setPublishYear(res.data.publishYear);
+        setLoading(false);
+      })
+      .catch((err) => {
+        setLoading(false);
+        alert("An error hapened.Please check console");
+        console.log(err);
+      });
+  }, []);
+
+  const handleEditBook = () => {
+    const data = {
+      title,
+      author,
+      publishYear,
+    };
+    setLoading(true);
+    axios
+      .put(`http://localhost:3000/books/${id}`, data)
+      .then(() => {
+        setLoading(false);
+        navigate("/");
+      })
+      .catch((err) => {
+        setLoading(false);
+        alert("An error hapened");
+        console.log(err);
+      });
+  };
+
+  return (
+    <div className="p-4">
+      <BackButton />
+      <h1 className="text-3xl my-4">Edit Book</h1>
+      {loading ? <SpinerComponent /> : ""}
+      <div className="flex flex-col border-2 border-sky-400 rounded-xl w-[600px] p-4 mx-auto">
+        <div className="p-4">
+          <label className="text-xl mr-4 text-gray-500 ">Title</label>
+          <input
+            type="text"
+            value={title}
+            onChange={(e) => setTitle(e.target.value)}
+            className="border-2 border-gray-500 px-4 py-2 w-full"
+          />
+        </div>
+        <div className="p-4">
+          <label className="text-xl mr-4 text-gray-500 ">Author</label>
+          <input
+            type="text"
+            value={author}
+            onChange={(e) => setAuthor(e.target.value)}
+            className="border-2 border-gray-500 px-4 py-2 w-full"
+          />
+        </div>
+        <div className="p-4">
+          <label className="text-xl mr-4 text-gray-500 ">Publish Year</label>
+          <input
+            type="text"
+            value={publishYear}
+            onChange={(e) => setPublishYear(e.target.value)}
+            className="border-2 border-gray-500 px-4 py-2 w-full"
+          />
+        </div>
+        <button className="p-2 bg-sky-300 m-8" onClick={handleEditBook}>
+          Edit Book
+        </button>
+      </div>
+    </div>
+  );
+};
 
 export default EditBook;
--- frontend/vite-project/src/services/BooksService.js
+++ frontend/vite-project/src/services/BooksService.js
@@ -3,8 +3,9 @@ import axios from "axios";
 class BookService {
   static getAllBooks = () => axios.get("/books");
   static showOneBook = (bookId) => axios.get(`/books/${bookId}`);
-  static createNewBook = (body) => axios.post("/books/", { body });
+  static createNewBook = (body) => axios.post("/books", body);
   static deleteBook = (deleteId) => axios.delete(`/books/${deleteId}`);
+  static editBook = (editId) => axios.get(`/books/${editId}`);
 }
 
 export default BookService;
